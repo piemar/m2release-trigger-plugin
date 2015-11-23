8f23ad7e6dea4be03dcad162412de7632dc8783f
piemar <pierre.m.petersson@gmail.com>
2015-11-23 11:30:33 +0100

    Added logging

--- src/main/java/com/baracuda/piepet/m2releasetrigger/M2ReleaseListener.java
+++ src/main/java/com/baracuda/piepet/m2releasetrigger/M2ReleaseListener.java
@@ -9,12 +9,16 @@ import hudson.model.listeners.RunListener;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by FPPE12 on 2015-11-22.
  */
 @Extension
 public class M2ReleaseListener extends RunListener<AbstractBuild> {
+    private static final Logger LOGGER = Logger.getLogger(M2ReleaseListener.class.getName());
+
     @Override
     public void onStarted(AbstractBuild abstractBuild, TaskListener listener) {
         super.onStarted(abstractBuild, listener);
@@ -37,12 +41,15 @@ public class M2ReleaseListener extends RunListener<AbstractBuild> {
     @Override
     public void onCompleted(AbstractBuild abstractBuild, TaskListener listener) {
         MavenModuleSetBuild mavenModuleSetBuild = ((MavenModuleSetBuild) abstractBuild);
-        abstractBuild.addAction(M2ReleaseTriggerBadge.createShortText(mavenModuleSetBuild.getBuildVariables().get("MVN_RELEASE_VERSION")));
+
         List<? extends Action> actions = mavenModuleSetBuild.getAllActions();
         for (Action action : actions) {
             if (action.getClass().getName().equals("org.jvnet.hudson.plugins.m2release.M2ReleaseArgumentsAction")) {
                 Object[] configurationList = (mavenModuleSetBuild.getProject().getPostbuilders().toMap()).values().toArray();
+
                 if (configurationList.length > 0) {
+                    LOGGER.log(Level.INFO, "Invoking M2Release post action");
+                    abstractBuild.addAction(M2ReleaseTriggerBadge.createShortText(mavenModuleSetBuild.getBuildVariables().get("MVN_RELEASE_VERSION")));
                     M2ReleasePostPlugin pomInterceptorPlugin = (M2ReleasePostPlugin) configurationList[0];
                     M2ReleasePostAction.trigger(pomInterceptorPlugin.getSystemId(), mavenModuleSetBuild.getBuildVariables().get("MVN_RELEASE_VERSION"), abstractBuild.getId(), pomInterceptorPlugin.getTargetEnvironments(),pomInterceptorPlugin.getPostReleaseAction());
                     break;
